b43222bad29a982a02fffbd9127febd4f85553cc
Angel Gladin <angelgladin@gmail.com>
2020-11-03 23:31:44 -0600

    Add SwipeToRefreshView and handle its events, map user request, and handle retry option

--- app/src/main/java/xyz/people/ui/main/MainFragment.kt
+++ app/src/main/java/xyz/people/ui/main/MainFragment.kt
@@ -10,12 +10,15 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.people.R
+import xyz.people.databinding.MainFragmentBinding
+import xyz.people.ui.main.entity.UserView
 import xyz.people.util.ResultState
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
+    private var binding: MainFragmentBinding? = null
 
     companion object {
         fun newInstance() = MainFragment()
@@ -28,23 +31,48 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = MainFragmentBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        setupUi()
+        setupObservers()
+    }
+
+    private fun setupObservers() {
         viewModel.getUser().observe(viewLifecycleOwner, Observer {
             when (it) {
                 is ResultState.Loading -> {
                     Log.e("loading", ":)")
                 }
                 is ResultState.Success -> {
+                    bindData(it.data)
                     Log.e("loading", it.toString())
                 }
                 is ResultState.Error -> {
                     Log.e("loading", ":)")
                 }
             }
+            binding?.swiperefresh!!.isRefreshing = false
         })
     }
 
+    private fun setupUi() {
+        binding?.let { it.swiperefresh.setOnRefreshListener {
+            viewModel.retry()
+        } }
+    }
+
+    private fun bindData(user: UserView) {
+        binding?.let { it.button.text = user.country }
+    }
 }
--- app/src/main/java/xyz/people/ui/main/MainViewModel.kt
+++ app/src/main/java/xyz/people/ui/main/MainViewModel.kt
@@ -1,9 +1,7 @@
 package xyz.people.ui.main
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
 import xyz.people.data.RandomUserService
 import xyz.people.ui.main.entity.UserView
@@ -13,27 +11,42 @@ class MainViewModel @ViewModelInject constructor(
     private val randomUserService: RandomUserService
 ) : ViewModel() {
 
-    fun getUser(): LiveData<ResultState<UserView>> = liveData(Dispatchers.IO) {
-        emit(ResultState.Loading())
-        try {
-            val response = randomUserService.getUser()
-            if (response.isSuccessful && response.body() != null) {
-                val userData = response.body()!!.results[0]
-                val user = UserView(
-                    userData.gender,
-                    "${userData.name.title} ${userData.name.first} ${userData.name.last}",
-                    userData.location.country,
-                    userData.location.coordinates.latitude,
-                    userData.location.coordinates.longitude,
-                    userData.email,
-                    userData.login.uuid,
-                    userData.picture.medium
-                )
+    private val reloadTrigger = MutableLiveData<Boolean>()
 
-                emit(ResultState.Success(user))
+    private var user: LiveData<ResultState<UserView>> = Transformations.switchMap(reloadTrigger) {
+        liveData<ResultState<UserView>>(Dispatchers.IO) {
+            emit(ResultState.Loading())
+            try {
+                val response = randomUserService.getUser()
+                if (response.isSuccessful && response.body() != null) {
+                    val userData = response.body()!!.results[0]
+                    val user = UserView(
+                        userData.gender,
+                        "${userData.name.title} ${userData.name.first} ${userData.name.last}",
+                        userData.location.country,
+                        userData.location.coordinates.latitude,
+                        userData.location.coordinates.longitude,
+                        userData.email,
+                        userData.login.uuid,
+                        userData.picture.medium
+                    )
+
+                    emit(ResultState.Success(user))
+                }
+            } catch (exception: Exception) {
+                emit(ResultState.Error(exception.message ?: "Unexpected error"))
             }
-        } catch (exception: Exception) {
-            emit(ResultState.Error(exception.message ?: "Unexpected error"))
         }
     }
+
+    init {
+        retry()
+    }
+
+    fun retry() {
+        reloadTrigger.value = true
+    }
+
+    fun getUser() = user
+
 }
