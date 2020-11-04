b85d99d8a8f6e594abd0e20d5b9087903acadc32
Angel Gladin <angelgladin@gmail.com>
2020-11-03 21:45:03 -0600

    Clean response

--- app/src/main/java/xyz/people/ui/main/MainViewModel.kt
+++ app/src/main/java/xyz/people/ui/main/MainViewModel.kt
@@ -6,19 +6,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import kotlinx.coroutines.Dispatchers
 import xyz.people.data.RandomUserService
-import xyz.people.data.entity.User
+import xyz.people.ui.main.entity.UserView
 import xyz.people.util.ResultState
 
 class MainViewModel @ViewModelInject constructor(
     private val randomUserService: RandomUserService
 ) : ViewModel() {
 
-    fun getUser(): LiveData<ResultState<User>> = liveData(Dispatchers.IO) {
+    fun getUser(): LiveData<ResultState<UserView>> = liveData(Dispatchers.IO) {
         emit(ResultState.Loading())
         try {
             val response = randomUserService.getUser()
             if (response.isSuccessful && response.body() != null) {
-                emit(ResultState.Success(response.body()!!))
+                val userData = response.body()!!.results[0]
+                val user = UserView(
+                    userData.gender,
+                    "${userData.name.title} ${userData.name.first} ${userData.name.last}",
+                    userData.location.country,
+                    userData.location.coordinates.latitude,
+                    userData.location.coordinates.longitude,
+                    userData.email,
+                    userData.login.uuid,
+                    userData.picture.medium
+                )
+
+                emit(ResultState.Success(user))
             }
         } catch (exception: Exception) {
             emit(ResultState.Error(exception.message ?: "Unexpected error"))
